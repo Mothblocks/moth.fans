98058f8597da9db5e99211a1174599498971ea21
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2023-12-01 18:02:29 -0800

    Fix get_results script, I don't know really why this broke

--- data-sections/nukes-n-flukes/get_results.sql
+++ data-sections/nukes-n-flukes/get_results.sql
@@ -9,17 +9,12 @@ FROM
         game_mode_result,
             server_port,
             FLOOR(DATEDIFF(NOW(), initialize_datetime) / 30) AS months_ago,
-            (SELECT
-                    id
-                FROM
-                    feedback
-                WHERE
-                    key_name = 'nuclear_challenge_mode'
-                        AND round_id = round.id) IS NOT NULL AS declared_war,
+            feedback.json IS NOT NULL AS declared_war,
             (game_mode_result = 'win - syndicate nuke'
                 OR game_mode_result = 'halfwin - syndicate nuke - did not evacuate in time') AS nuked
     FROM
         round
+	LEFT JOIN feedback ON feedback.round_id = round.id AND key_name = 'nuclear_challenge_mode'
     WHERE
         (game_mode_result = 'loss - syndicate nuked - disk secured'
             OR game_mode_result = 'win - syndicate nuke'
@@ -32,5 +27,6 @@ FROM
             OR game_mode_result = 'halfwin - detonation averted'
             OR game_mode_result = 'halfwin - interrupted')
             AND initialize_datetime >= (NOW() - INTERVAL 6 MONTH)
-    ORDER BY id DESC) t
+    ORDER BY round.id DESC) t
 GROUP BY server_port , months_ago , declared_war , nuked
+
